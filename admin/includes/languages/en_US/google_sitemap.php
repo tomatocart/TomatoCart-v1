8fbb889eac5f0a5f22a5135328faeff7d7e7d411
tocjack <jack.yin@tomatocart.com>
2013-08-15 16:55:30 +0800

    [#67]Sitemap bug. - using only admin panel language in links.
    Modify the warining message in the panel

--- admin/includes/languages/en_US/google_sitemap.php
+++ admin/includes/languages/en_US/google_sitemap.php
@@ -12,7 +12,7 @@ heading_title = Google Sitemaps
 
 action_heading_google_sitemaps_generation = Google Sitemaps Generation
 
-introduction_create_google_sitemaps = <p><b>NOTE:</b> Please ensure your tomatocart directory is writable to create the sitemaps for each language except english. After the creation, you should make it unwritable for the security.</p>
+introduction_create_google_sitemaps = <p><b>NOTE:</b> Please ensure your tomatocart directory is writable to create the sitemaps for each language except english. After the creation, you should make it unwritable for the security.</p><p>Be careful with non-ascii characters in frienld URLs for products, categories and articles because they are not the standard characters for url. Only characters you can reliably use for the actual name parts of a URL are a-z, A-Z, 0-9, -, ., _, and ~. Any other characters may result in problem.</p>
 
 introduction_google_sitemaps_submission  = <b>NOTE:</b> The location of the Sitemap is included in the robots.txt file so that Google and other search engines know about your sitemap. But Google still recommend that the sitemaps should be submitted through Google Webmaster Tools account so you can make sure that the Sitemap was processed without any issues, and to get additional statistics about your site. <br/><br/>Please ensure that you has registered with Google Sitemaps, and submitted your initial sitemap before proceeding the following step.
 
