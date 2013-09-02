3366827b11e2229e2968ac3ece84b686ba347867
tocjack <jack.yin@tomatocart.com>
2013-09-02 16:00:27 +0800

    [#130]Store template don't change automatically after changing the default template

--- includes/classes/template.php
+++ includes/classes/template.php
@@ -805,30 +805,44 @@
  */
 
     function set($code = null) {
-      if ( (isset($_SESSION['template']) === false) || !empty($code) || (isset($_GET['template']) && !empty($_GET['template'])) ) {
-        if ( !empty( $code ) ) {
-          $set_template = $code;
-        } else {
-          $set_template = (isset($_GET['template']) && !empty($_GET['template'])) ? $_GET['template'] : DEFAULT_TEMPLATE;
-        }
+    	//use the template box or template code to change the template
+    	$set_template = null;
+		  if ( (!empty($code)) || (isset($_GET['template']) && !empty($_GET['template'])) ) {
+		  	if (!empty($code)) {
+	  			$set_template = $code;
+		  	}else {
+		  		$set_template = $_GET['template'];
+		  	}
+		  	
+		  	$_SESSION['change_template'] = true;
+		  }
+		  
+			//there isn't any template set before or the default template is changed
+		  if (!isset($_SESSION['change_template'])) {
+		  	if ( (isset($_SESSION['template']) === false) || ($_SESSION['template'] != DEFAULT_TEMPLATE) ) {
+		  		$set_template = DEFAULT_TEMPLATE;
+		  	}
+		  }
 
-        $data = array();
-        $data_default = array();
-
-        foreach ($this->getTemplates() as $template) {
-          if ($template['code'] == DEFAULT_TEMPLATE) {
-            $data_default = array('id' => $template['id'], 'code' => $template['code']);
-          } elseif ($template['code'] == $set_template) {
-            $data = array('id' => $template['id'], 'code' => $template['code']);
-          }
-        }
-
-        if (empty($data)) {
-          $data =& $data_default;
-        }
-
-        $_SESSION['template'] =& $data;
-      }
+		  //the template need to be set in the session or changed
+		  if ($set_template !== null) {
+		  	$data = array();
+		  	$data_default = array();
+		  	
+		  	foreach ($this->getTemplates() as $template) {
+		  		if ($template['code'] == DEFAULT_TEMPLATE) {
+		  			$data_default = array('id' => $template['id'], 'code' => $template['code']);
+		  		} elseif ($template['code'] == $set_template) {
+		  			$data = array('id' => $template['id'], 'code' => $template['code']);
+		  		}
+		  	}
+		  	
+		  	if (empty($data)) {
+		  		$data =& $data_default;
+		  	}
+		  	
+		  	$_SESSION['template'] =& $data;
+		  }
 
       $this->_template_id =& $_SESSION['template']['id'];
       $this->_template =& $_SESSION['template']['code'];
