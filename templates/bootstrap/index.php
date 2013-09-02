1b07731e1877a9a69edc039e657fe43d83d9e906
tocjack <jack.yin@tomatocart.com>
2013-09-02 16:39:20 +0800

    [#128][Bootstrap] Slideshow did not start automatically
    -- Now, the slide images will be slided automatically with 
    interval: 3000

--- templates/bootstrap/index.php
+++ templates/bootstrap/index.php
@@ -368,7 +368,9 @@ window.addEvent('domready', function() {
 if (typeof jQuery != 'undefined') {
     (function($) {
         $(document).ready(function(){
-            $('.carousel').each(function(index, element) {
+            $('.carousel').carousel({
+                interval: 3000
+            }).each(function(index, element) {
             	$(this)[index].slide = null;
             });
         });
