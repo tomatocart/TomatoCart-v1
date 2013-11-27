1f24bc99eac805fad13e0a0ed0f210cb10669d95
tocjack <jack.yin@tomatocart.com>
2013-11-27 16:48:31 +0800

    [#190]
    -- Add the variants dropdown into the product listing
    
    -- When change the variant, the product price and image will 
    be updated
    
    -- If the variant product is not available, an warning message
    will be displayed.
    
    Next step, i will impelement the add to cart action for it.

---
+++ includes/javascript/list_variants.js
@@ -0,0 +1,262 @@
+/*
+  $Id: list_variants.js $
+  TomatoCart Open Source Shopping Cart Solutions
+  http://www.tomatocart.com
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License v2 (1991)
+  as published by the Free Software Foundation.
+ */
+var TocListVariants = new Class({
+    Implements : [ Options ],
+    options : {
+        hasSpecial: 0,
+        remoteUrl: 'json.php',
+        combVariants: null,
+        productsId: null,
+        linkCompareProductsCls: '.compare',
+        linkWishlistCls : '.wishlist',
+        priceCls: '.price',
+        buyElCls: '.buyBlock',
+        lang : {
+            txtInStock : 'In Stock',
+            txtOutOfStock : 'Out Of Stock',
+            txtNotAvailable : 'Not Available'
+        }
+    },
+
+    /**
+     * Initialize
+     * 
+     * @access public
+     * @params object the options for this class
+     * 
+     * return void
+     */
+    initialize : function(options) {
+        this.setOptions(options);
+        
+        if (this.options.combVariants !== null && this.options.combVariants.length > 0) {
+            this.checkCompareProducts();
+            this.checkWishlist();
+            this.initializeComboBox();
+        }
+        
+        if (this.options.productsId != null) {
+            this.imgEl = $('img_ac_productlisting_' + this.options.productsId).getElement('img.productImage');
+        }
+    },
+
+    /**
+     * Initialize the combobox
+     * 
+     * @access private
+     * 
+     * return void
+     */
+    initializeComboBox : function() {
+        this.options.combVariants.each(function(combobox) {
+            combobox.addEvent('change', function() {
+                this.updateView(combobox);
+            }.bind(this));
+        }.bind(this));
+    },
+
+    /**
+     * Check the compare products links
+     * 
+     * @access private
+     * 
+     * return void
+     */
+    checkCompareProducts : function() {
+        var linkCp = $$(this.options.linkCompareProductsCls);
+
+        if (linkCp.length > 0) {
+            this.linkCp = linkCp[0];
+            this.linkCpHref = this.linkCp.getProperty('href');
+
+            if (this.linkCpHref.search(/cid=/) !== -1) {
+                this.linkCpHref = this.linkCpHref.replace(/&cid=\d+/, '');
+            }
+        }
+    },
+
+    /**
+     * Check the wishlist link
+     * 
+     * @access pricate
+     * 
+     * return void
+     */
+    checkWishlist : function() {
+        var linkWp = $$(this.options.linkWishlistCls);
+
+        if (linkWp.length > 0) {
+            this.linkWp = linkWp[0];
+            this.linkWpHref = this.linkWp.getProperty('href');
+
+            if (this.linkWpHref.search(/wid=/) !== -1) {
+                this.linkWpHref = this.linkWpHref.replace(/&wid=\d+/, '');
+            }
+        }
+    },
+
+    /**
+     * Update the product row
+     * 
+     * @access priate
+     * @param element
+     * 
+     * return void
+     */
+    updateView : function(combobox) {
+        var productsIdString = this.getProductsIdString(),
+            product = this.options.variants[productsIdString],
+            buyEl = combobox.getParent(this.options.buyElCls),
+            selects = buyEl.getElements('select'),
+            href,
+            error;
+            
+        // if it is in the product info page and the product have any variants,
+        // add the variants into the compare products link
+        if (this.linkCp) {
+            href = this.linkCpHref + '&cid='
+                    + productsIdString.replace(/#/, '_');
+
+            this.linkCp.setProperty('href', href);
+        }
+
+        // handler the wishlist
+        if (this.linkWp) {
+            href = this.linkWpHref + '&wid='
+                    + productsIdString.replace(/#/, '_');
+
+            this.linkWp.setProperty('href', href);
+        }
+
+        //check whether the variant product is available
+        if (product == undefined || (product['status'] == 0)) {
+            //create error element and inject into the top of the buy element
+            if (!error) {
+                error = new Element('div', {
+                    'html': '<em>' + this.options.lang.txtNotAvailable + '</em>',
+                    'class': 'warning'
+                });
+            }
+            
+            error.inject(buyEl, 'top');
+            
+            //add the alert mark for the selects
+            selects.addClass('alert');
+            
+            //remove the animated class for the product image
+            if (this.imgEl) {
+                if (this.imgEl.hasClass('animated')) {
+                    this.imgEl.removeClass('animated flash');
+                }
+            }
+        } else {
+            //remove the alert mark for the selects
+            selects.removeClass('alert');
+            
+            //destroy the error element
+            if (buyEl.getElement('.warning')) {
+                buyEl.getElement('.warning').destroy();
+            }
+            
+            if (this.options.hasSpecial == 0) {
+                // get the formatted price of the variants product by ajax request
+                this.sendRequest({
+                    action : 'get_variants_formatted_price',
+                    products_id_string : productsIdString
+                }, function(response) {
+                    var result = JSON.decode(response);
+
+                    if (result.success == true) {
+                        combobox.closest(this.options.priceCls).set('html', '<span class="animated flash">' + result.formatted_price + '</span>');
+                    } else {
+                        alert(result.feedback);
+                    }
+                }.bind(this));
+            }
+
+            this.changeImage(product['image'], combobox);
+        }
+    },
+
+    /**
+     * Change the product image
+     * 
+     * @access private
+     * @param string new image name need to be changed
+     * 
+     * return boolean
+     */
+    changeImage: function(image) {
+        var imgsrc, 
+            path = [];
+        
+        if (this.options.productsId != null) {
+            //change the src path
+            if (this.imgEl) {
+                imgSrc = this.imgEl.getProperty('src');
+                if (imgSrc) {
+                    path = imgSrc.split('/');
+                    if (path.length > 0) {
+                        path[path.length - 1] = image;
+                        
+                        imgSrc = path.join('/');
+                        
+                        this.imgEl.setProperty('src', imgSrc).addClass('animated flash');
+                        
+                        return true;
+                    }
+                }
+            }
+        }
+        
+        return false;
+    },
+    
+    /**
+     * Get the prodcut id string based on the product id and variants
+     * 
+     * @access private
+     * 
+     * return string
+     */
+    getProductsIdString: function() {
+        var groups = [],
+            id,
+            groupsID;
+        
+        this.options.combVariants.each(function(combobox) {
+            id = combobox.id.toString();
+            groupsID = id.substring(9, id.indexOf(']'));
+          
+            groups.push(groupsID + ':' + combobox.value);
+        }.bind(this));
+        
+        return this.options.productsId + '#' + groups.join(';');
+    },
+    
+    /**
+     * Send the ajax request
+     * 
+     * @access private
+     * @param object request params to be passed to the server
+     * @param function callback for the successful request
+     * 
+     * return void
+     */
+    sendRequest : function(data, fnSuccess) {
+        data.module = 'products';
+
+        var loadRequest = new Request({
+            url : this.options.remoteUrl,
+            data : data,
+            onSuccess : fnSuccess.bind(this)
+        }).send();
+    }
+});
--- includes/modules/product_listing.php
+++ includes/modules/product_listing.php
@@ -10,6 +10,11 @@
   it under the terms of the GNU General Public License v2 (1991)
   as published by the Free Software Foundation.
 */
+  //load the language for the variants products
+  $osC_Language->load('products');
+  
+  //collect the product objects
+  $collections = array();
 
 // create column list
   $define_list = array('PRODUCT_LIST_SKU' => PRODUCT_LIST_SKU, 
@@ -114,6 +119,7 @@
 
       for ($col=0, $n=sizeof($column_list); $col<$n; $col++) {
         $lc_align = '';
+        $lc_class = '';
 
         switch ($column_list[$col]) {
           case 'PRODUCT_LIST_SKU':
@@ -137,8 +143,11 @@
             break;
           case 'PRODUCT_LIST_PRICE':
             $lc_align = 'right';
+            $lc_class .= ' price';
             $osC_Product = new osC_Product($Qlisting->value('products_id'));
             $lc_text = $osC_Product->getPriceFormated(true);
+            
+            $collections[] = $osC_Product;
             break;
           case 'PRODUCT_LIST_QUANTITY':
             $lc_align = 'right';
@@ -167,23 +176,39 @@
           case 'PRODUCT_LIST_BUY_NOW':
             $lc_align = 'center';
             
+            $lc_text = '';
+            if ($osC_Product->hasVariants()) {
+							//change the flag
+							$lc_text .= '<div class="buyBlock variants_' . $osC_Product->getID() . '">';
+            	$combobox_array = $osC_Product->getVariantsComboboxArray();
+            
+            	foreach ($combobox_array as $groups_name => $combobox) {
+								$lc_text .= '<div class="variant">';
+            		$lc_text .=  '<label>' . $groups_name . ':</label>';
+                $lc_text .= $combobox;
+                $lc_text .= '</div>';
+            	}
+            	
+            	$lc_text .= '</div>';
+            }
+            
             if ($Qlisting->value('products_type') == PRODUCT_TYPE_SIMPLE) {
-							$lc_text = '<input type="text" id="qty_' . $Qlisting->value('products_id') . '" value="1" size="1" class="qtyField" />';
+							$lc_text .= '<input type="text" id="qty_' . $Qlisting->value('products_id') . '" value="1" size="1" class="qtyField" />';
               $lc_text .= osc_link_object(osc_href_link(basename($_SERVER['SCRIPT_FILENAME']), $Qlisting->value('products_id') . '&' . osc_get_all_get_params(array('action')) . '&action=cart_add'), osc_draw_image_button('button_buy_now.gif', $osC_Language->get('button_buy_now'), 'class="ajaxAddToCart" id="ac_productlisting_'. $Qlisting->value('products_id') . '"')) . '&nbsp;<br />';
             }else {
-              $lc_text = osc_link_object(osc_href_link(basename($_SERVER['SCRIPT_FILENAME']), $Qlisting->value('products_id') . '&' . osc_get_all_get_params(array('action')) . '&action=cart_add'), osc_draw_image_button('button_buy_now.gif', $osC_Language->get('button_buy_now'))) . '&nbsp;<br />';
+              $lc_text .= osc_link_object(osc_href_link(basename($_SERVER['SCRIPT_FILENAME']), $Qlisting->value('products_id') . '&' . osc_get_all_get_params(array('action')) . '&action=cart_add'), osc_draw_image_button('button_buy_now.gif', $osC_Language->get('button_buy_now'))) . '&nbsp;<br />';
             }
             
             if ($osC_Template->isInstalled('compare_products', 'boxes')) {
-              $lc_text .= osc_link_object(osc_href_link(basename($_SERVER['SCRIPT_FILENAME']), 'cid=' . $Qlisting->value('products_id') . '&' . osc_get_all_get_params(array('action')) . '&action=compare_products_add'), $osC_Language->get('add_to_compare')) . '&nbsp;<br />';
+              $lc_text .= osc_link_object(osc_href_link(basename($_SERVER['SCRIPT_FILENAME']), 'cid=' . $Qlisting->value('products_id') . '&' . osc_get_all_get_params(array('action')) . '&action=compare_products_add'), $osC_Language->get('add_to_compare'), 'class="compare"') . '&nbsp;<br />';
             }  
             
-            $lc_text .= osc_link_object(osc_href_link(basename($_SERVER['SCRIPT_FILENAME']), $Qlisting->value('products_id') . '&' . osc_get_all_get_params(array('action')) . '&action=wishlist_add'), $osC_Language->get('add_to_wishlist')); 
+            $lc_text .= osc_link_object(osc_href_link(basename($_SERVER['SCRIPT_FILENAME']), $Qlisting->value('products_id') . '&' . osc_get_all_get_params(array('action')) . '&action=wishlist_add'), $osC_Language->get('add_to_wishlist'), 'class="wishlist"'); 
                         
             break;
         }
 
-        echo '      <td ' . ((empty($lc_align) === false) ? 'align="' . $lc_align . '" ' : '') . ' valign="top" class="productListing-data">' . $lc_text . '</td>' . "\n";
+        echo '      <td ' . ((empty($lc_align) === false) ? 'align="' . $lc_align . '" ' : '') . ' valign="top" class="productListing-data' . $lc_class . '">' . $lc_text . '</td>' . "\n";
       }
 
       echo '    </tr>' . "\n";
@@ -212,4 +237,31 @@
 
 <?php
   }
+?>
+
+<script type="text/javascript" src="includes/javascript/list_variants.js"></script>
+
+<?php 
+	if (count($collections) > 0) {
+		foreach ($collections as $product) {
+			if ($product->hasVariants()) {
+?>
+				<script type="text/javascript">
+					new TocListVariants({
+				    remoteUrl: '<?php echo osc_href_link('json.php', null, 'SSL', false, false, true); ?>',
+				    combVariants: $$('.variants_<?php echo $product->getID(); ?> select'),
+				    variants: <?php echo $toC_Json->encode($product->getVariants()); ?>,
+				    productsId: <?php echo $product->getID(); ?>,
+				    hasSpecial: <?php echo $product->hasSpecial() ? 1 : 0; ?>,
+				    lang: {
+				      txtInStock: '<?php echo addslashes($osC_Language->get('in_stock'));?>',
+				      txtOutOfStock: '<?php echo addslashes($osC_Language->get('out_of_stock')); ?>',
+				      txtNotAvailable: '<?php echo addslashes($osC_Language->get('not_available')); ?>'
+				    }
+				  });
+				</script>
+<?php
+			} 
+		}
+	}
 ?>
